316a0623ac6e96e18ab88131f0e1378fa3eace49
ITIT <ITIT@ULSAs-iMac.local>
2023-11-16 18:37:55 -0600

    Se agregan las vistas de login y register y se conecta al backend

---
+++ src/AppLogin.vue
@@ -0,0 +1,85 @@
+<template>
+  <div id="app">
+    <b-container>
+      <b-row>
+        <b-col></b-col>
+        <b-col cols="6">
+          <b-card title="Login">
+            <b-form @submit="login">
+              <b-form-group label="Username:">
+                <b-form-input v-model="username" placeholder="Enter your username" required>
+                </b-form-input>
+              </b-form-group>
+
+              <b-form-group label="Password:">
+                <b-form-input v-model="pass" type="password" placeholder="Enter your password" required>
+                </b-form-input>
+              </b-form-group>
+
+
+              <b-button type="submit" variant="primary">Login</b-button>
+
+            </b-form>
+          </b-card>
+
+        </b-col>
+        <b-col></b-col>
+      </b-row>
+    </b-container>
+
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  components: {
+
+  },
+  data() {
+    return {
+      username: "",
+      pass: ""
+    }
+  },
+  methods: {
+    async login(event) {
+     
+      event.preventDefault();
+      const requestBody = {
+        usrn: this.username,
+        password: this.pass
+      }
+      try {
+        const serverUrl = "https://tame-red-cockatoo-tie.cyclic.app/users/iniciar_sesion";
+        const response = await axios.post(serverUrl, requestBody);
+        console.log(response)
+        alert("Haz iniciado sesión!!")
+      } catch (error) {
+        console.log(error)
+        alert("Inicio de sesion erroneo")
+      }
+
+    }
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+
+
+
---
+++ src/AppRegister.vue
@@ -0,0 +1,93 @@
+<template>
+    <div id="app">
+      <b-container>
+        <b-row>
+          <b-col></b-col>
+          <b-col cols="6">
+            <b-card title="Register">
+              <b-form @submit="register">
+  
+                <b-form-group label="Nombre:">
+                  <b-form-input v-model="firstName" placeholder="Enter your first name" required>
+                  </b-form-input>
+                </b-form-group>
+  
+                <b-form-group label="Apellido:">
+                  <b-form-input v-model="lastName" placeholder="Enter your last name" required>
+                  </b-form-input>
+                </b-form-group>
+  
+                <b-form-group label="Matrícula:">
+                  <b-form-input v-model="matricula" placeholder="Enter your matricula" required>
+                  </b-form-input>
+                </b-form-group>
+  
+                <b-form-group label="Username:">
+                  <b-form-input v-model="username" placeholder="Enter your username" required>
+                  </b-form-input>
+                </b-form-group>
+  
+                <b-form-group label="Password:">
+                  <b-form-input v-model="password" type="password" placeholder="Enter your password" required>
+                  </b-form-input>
+                </b-form-group>
+  
+                <b-button type="submit" variant="primary">Register</b-button>
+  
+              </b-form>
+            </b-card>
+          </b-col>
+          <b-col></b-col>
+        </b-row>
+      </b-container>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    name: 'Register-component',
+    data() {
+      return {
+        firstName: "",
+        lastName: "",
+        matricula: "",
+        username: "",
+        password: ""
+      }
+    },
+    methods: {
+      async register(event) {
+        event.preventDefault();
+        const requestBody = {
+          name: this.firstName,
+          surname: this.lastName,
+          tuition: this.matricula,
+          usrn: this.username,
+          password: this.password
+        }
+        try {
+          const serverUrl = "https://tame-red-cockatoo-tie.cyclic.app/users/registrar";
+          const response = await axios.post(serverUrl, requestBody);
+          console.log(response)
+          alert("Registro exitoso!")
+        } catch (error) {
+          console.log(error)
+          alert("Error en el registro")
+        }
+      }
+    }
+  }
+  </script>
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  </style>
+  
